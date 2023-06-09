a668db2b011abf7b7603a1178c361004fe249327
rohanchau1111 <chaudharirohan938@gmail.com>
2023-06-09 12:37:23 +0530

    Modify

--- src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Welcome to Rohan... You are in K8s with argo C!!";
+		return "Welcome to Rohan... You are in K8s with argo Chaudhari!!";
 	}
 
 	public static void main(String[] args) {
